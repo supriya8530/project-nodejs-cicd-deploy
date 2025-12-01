dc650625adbe000c48b101504d48a20fce940bb9
supriyas-devops <supriyarupnar99999@gmail.com>
2025-12-01 17:19:10 +0530

    second commite

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("🚀 Node.js CI/CD deployment successful!");
+  res.send("nodejs project developed  by supriya rupnar");
 });
 
 app.listen(port, () => {
