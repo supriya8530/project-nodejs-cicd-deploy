3dd79735a632f314f338f98f62b9fc8bc9d7848b
supriyas-devops <supriyarupnar99999@gmail.com>
2025-12-01 17:28:30 +0530

    updated projects

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("nodejs project developed  by supriya rupnar");
+  res.send("nodejs project developed  by supriya rupnar this is cicd pipeline");
 });
 
 app.listen(port, () => {
